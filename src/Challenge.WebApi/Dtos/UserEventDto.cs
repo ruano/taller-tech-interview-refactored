9e50dcfd67f18d9d5b23e98894020f90001d3946
Ruano Martinez Schulze <ruano.martinez@gmail.com>
2025-10-22 20:10:03 -0300

    feat: Initialize Challenge.WebApi project
    Set up initial ASP.NET Core Web API project with Swagger support, basic launch settings, and configuration files. Added UserEventDto and a POST /events endpoint for event creation.

---
+++ src/Challenge.WebApi/Dtos/UserEventDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Challenge.WebApi.Dtos;
+
+public record UserEventDto
+{
+    public string UserId { get; set; }
+    public string Type { get; set; }
+    public DateTime? TimeStamp { get; set; }
+    public string? Data { get; set; }
+}
